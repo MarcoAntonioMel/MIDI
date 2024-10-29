eda6f5415447e9105bde45096b9232be3239c5dd
MarcoAntonioMel <antoniomel2504@gmail.com>
2024-10-29 15:06:18 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+function playSonidoPom () {
+    document.querySelector('#sonido_tecla_pom').play();
+}
+document.querySelector('.tecla_pom').onclick = playSonidoPom;
+
+
+function playSonidoClap () {
+    document.querySelector('#sonido_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclick = playSonidoClap;
+
+
+function playSonidoTim () {
+    document.querySelector('#sonido_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclick = playSonidoTim;
+
+
+function playSonidoPuff () {
+    document.querySelector('#sonido_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclick = playSonidoPuff;
+
+
+function playSonidoSplash () {
+    document.querySelector('#sonido_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclick = playSonidoSplash;
+
+
+function playSonidoToim () {
+    document.querySelector('#sonido_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclick = playSonidoToim;
+
+
+function playSonidoPsh () {
+    document.querySelector('#sonido_tecla_psh').play();
+}
+document.querySelector('.tecla_psh').onclick = playSonidoPsh;
+
+
+function playSonidoTic () {
+    document.querySelector('#sonido_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclick = playSonidoTic;
+
+
+function playSonidoTom () {
+    document.querySelector('#sonido_tecla_tom').play();
+}
+document.querySelector('.tecla_tom').onclick = playSonidoTom;

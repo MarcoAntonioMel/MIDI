ddb614b7eb0a28853b65ef4907345b8dd79cdbd3
MarcoAntonioMel <antoniomel2504@gmail.com>
2024-10-29 18:10:31 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,52 +1,35 @@
-function playSonidoPom () {
-    document.querySelector('#sonido_tecla_pom').play();
+function playSonido (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_pom').onclick = playSonidoPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
-function playSonidoClap () {
-    document.querySelector('#sonido_tecla_clap').play();
+for(let contador = 0; contador < listaDeTeclas.length; contador++ ){
+    const tecla = listaDeTeclas [contador];
+    const instrumento = tecla.classList[1];
+    console.log(contador)
+
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+    
+    tecla.onclick = function (){
+        playSonido(idAudio);
+    };
+    tecla.onkeydown = function (evento){
+
+        if ( evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa');
+        }      
+        console.log()
+        
+
+    }
+    tecla.onkeyUp = function (){
+        tecla.classList.remove('activa');
+    }
+
 }
-document.querySelector('.tecla_clap').onclick = playSonidoClap;
 
 
-function playSonidoTim () {
-    document.querySelector('#sonido_tecla_tim').play();
-}
-document.querySelector('.tecla_tim').onclick = playSonidoTim;
 
-
-function playSonidoPuff () {
-    document.querySelector('#sonido_tecla_puff').play();
-}
-document.querySelector('.tecla_puff').onclick = playSonidoPuff;
-
-
-function playSonidoSplash () {
-    document.querySelector('#sonido_tecla_splash').play();
-}
-document.querySelector('.tecla_splash').onclick = playSonidoSplash;
-
-
-function playSonidoToim () {
-    document.querySelector('#sonido_tecla_toim').play();
-}
-document.querySelector('.tecla_toim').onclick = playSonidoToim;
-
-
-function playSonidoPsh () {
-    document.querySelector('#sonido_tecla_psh').play();
-}
-document.querySelector('.tecla_psh').onclick = playSonidoPsh;
-
-
-function playSonidoTic () {
-    document.querySelector('#sonido_tecla_tic').play();
-}
-document.querySelector('.tecla_tic').onclick = playSonidoTic;
-
-
-function playSonidoTom () {
-    document.querySelector('#sonido_tecla_tom').play();
-}
-document.querySelector('.tecla_tom').onclick = playSonidoTom;

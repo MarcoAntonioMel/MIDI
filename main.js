a68f5bb2314d0b5dca75222f1b53984354a33866
MarcoAntonioMel <antoniomel2504@gmail.com>
2024-10-29 18:14:06 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,34 @@
-function playSonido (idElementoAudio) {
+function playSonido (idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
-for(let contador = 0; contador < listaDeTeclas.length; contador++ ){
-    const tecla = listaDeTeclas [contador];
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     console.log(contador)
 
     const idAudio = `#sonido_${instrumento}`;
     console.log(idAudio)
-    
+
     tecla.onclick = function (){
         playSonido(idAudio);
     };
-    tecla.onkeydown = function (evento){
-
-        if ( evento.code === 'Space' || evento.code === 'Enter'){
-            tecla.classList.add('activa');
-        }      
-        console.log()
+    tecla.onkeydown = function(evento){
         
-
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa');
+        }
+        console.log(evento.code === 'Space' || evento.code === 'Enter')
+       
+        
     }
-    tecla.onkeyUp = function (){
+    tecla.onkeyup = function(){
         tecla.classList.remove('activa');
     }
+   
 
 }
 
